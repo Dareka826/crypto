40f52716e2a919d17711a85648f03a7942d4ac65
Rin Brk <dareka826@gmail.com>
2023-11-30 22:07:15 +0100

    A different approach to collision test

--- collision_test.sh
+++ collision_test.sh
@@ -2,6 +2,5 @@
 
 truncate -s0 hashes.txt
 
-tclsh ./seq.tcl | \
-    xargs -d'\n' -L1 dash -c 'printf "%s" "$1" | ./cchash' '' | \
-    tee hashes.txt
+tr -cd 'a-zA-Z0-9,.?! ' </dev/random | fold -w 1000 | head -n 1000 | \
+    xargs -d'\n' -L1 dash -c 'printf "%s" "$1" | ./cchash' '' >>hashes.txt
