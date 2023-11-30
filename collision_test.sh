1f259410f9403a392ac51271d5555a94de47b5fe
Rin Brk <dareka826@gmail.com>
2023-11-30 22:15:12 +0100

    Update collision tests

--- collision_test.sh
+++ collision_test.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
+set -eu
 
 truncate -s0 hashes.txt
+LINES="10000"
 
-tr -cd 'a-zA-Z0-9,.?! ' </dev/random | fold -w 1000 | head -n 1000 | \
-    xargs -d'\n' -L1 dash -c 'printf "%s" "$1" | ./cchash' '' >>hashes.txt
+for i in $(seq ${LINES}); do
+    if [ "$(( i % 100 ))" = 0 ]; then
+        printf "[%d/%d = %d%%]\n" "${i}" "${LINES}" "$(( 100 * i / LINES ))"
+    fi
+
+    RAND="$(dd if=/dev/random count=4 bs=1 2>/dev/null | od -A n -t u)"
+    LEN="$(( RAND % 999 + 1 ))"
+
+    tr -cd 'a-zA-Z0-9,.?! ' </dev/random | fold -w "${LEN}" | head -n 1 | \
+        xargs -d'\n' -L1 dash -c 'printf "%s" "$1" | ./cchash' '' >>hashes.txt
+done
+
+# Check collisions
+printf "Collisions: %s\n" "$(sort <hashes.txt | uniq -c | grep -v '^\s*1 ' | sort -n | wc -l)"
